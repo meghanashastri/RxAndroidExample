9562dc5085d4e6c8682f87d37c8df22bb2d78003
meghana <meghanashastri07@gmail.com>
2016-08-05 14:48:33 +0530

    Used operators with observables

--- app/src/main/java/com/example/ubuntu/rxandroidexample/MainActivity.java
+++ app/src/main/java/com/example/ubuntu/rxandroidexample/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.observers.Observers;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,36 +18,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> myObservable = observable();
-        Observer<String> myObserver = observer();
-        Subscription mySubscription = myObservable.subscribe(myObserver);
-        mySubscription.unsubscribe();
+        observableToGetSingleIntegers();
+        observableToGetSquares();
+        getSelectedIntegers();
     }
 
-    private Observable<String> observable(){
-        Observable<String> myObservable
-                = Observable.just("Hello"); // Emits "Hello"
-        return myObservable;
+    private void observableToGetSingleIntegers(){
+        Observable<Integer> myArrayObservable
+                = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6}); // Emits each item of the
+        // array, one at a time
+
+        myArrayObservable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer i) {
+                Log.d("My Action", String.valueOf(i)); // Prints the number received
+            }
+        });
     }
 
-    private Observer<String> observer(){
-        Observer<String> myObserver = new Observer<String>() {
-            @Override
-            public void onCompleted() {
-                // Called when the observable has no more data to emit
-            }
+    private void observableToGetSquares(){
+        Observable<Integer> myArrayObservable
+                = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6}); // Emits each item of the
+        // array, one at a time
 
+        myArrayObservable = myArrayObservable.map(new Func1<Integer, Integer>() {
+            // Input and Output are both Integer
             @Override
-            public void onError(Throwable e) {
-                // Called when the observable encounters an error
+            public Integer call(Integer integer) {
+                return integer * integer; // Square the number
             }
+        });
 
+         myArrayObservable.subscribe(new Action1<Integer>() {
             @Override
-            public void onNext(String s) {
-                // Called each time the observable emits data
-                Log.d("MY OBSERVER", s);
+            public void call(Integer i) {
+                Log.d("My Action", String.valueOf(i)); // Prints the number received
             }
-        };
-        return myObserver;
+        });
+    }
+
+    private void getSelectedIntegers(){
+
+        Observable<Integer> myArrayObservable
+                = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6}); // Emits each item of the
+        // array, one at a time
+
+        myArrayObservable
+                .skip(2) // Skip the first two items
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) { // Ignores any item that returns false
+                        return integer % 2 == 0;
+                    }
+                });
+              // Emits 4 and 6
+
     }
 }
