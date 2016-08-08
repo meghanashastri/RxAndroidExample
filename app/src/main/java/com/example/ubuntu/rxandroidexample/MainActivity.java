b84546e1228683990b0aff92d27ed1c65b4f8c09
meghana <meghanashastri07@gmail.com>
2016-08-08 11:19:46 +0530

    click event added to button using observables

--- app/src/main/java/com/example/ubuntu/rxandroidexample/MainActivity.java
+++ app/src/main/java/com/example/ubuntu/rxandroidexample/MainActivity.java
@@ -3,49 +3,43 @@ package com.example.ubuntu.rxandroidexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
+import rx.android.view.OnClickEvent;
+import rx.android.view.ViewObservable;
+import rx.functions.Action1;
 import rx.observers.Observers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> myObservable = observable();
-        Observer<String> myObserver = observer();
-        Subscription mySubscription = myObservable.subscribe(myObserver);
-        mySubscription.unsubscribe();
+        btnClickListener();
     }
 
-    private Observable<String> observable(){
-        Observable<String> myObservable
-                = Observable.just("Hello"); // Emits "Hello"
-        return myObservable;
-    }
+    private void btnClickListener(){
+        btnClickMe = (Button) findViewById(R.id.btnClickMe);
 
-    private Observer<String> observer(){
-        Observer<String> myObserver = new Observer<String>() {
-            @Override
-            public void onCompleted() {
-                // Called when the observable has no more data to emit
-            }
+        Observable<OnClickEvent> clicksObservable
+                = ViewObservable.clicks(btnClickMe); // Create a ViewObservable for the Button
 
-            @Override
-            public void onError(Throwable e) {
-                // Called when the observable encounters an error
-            }
+        clicksObservable
+                .skip(4)    // Skip the first 4 clicks
+                .subscribe(new Action1<OnClickEvent>() {
+                    @Override
+                    public void call(OnClickEvent onClickEvent) {
+                        Log.d("Click Action", "Clicked!");
+                        // Printed from the fifth click onwards
+                    }
+                });
 
-            @Override
-            public void onNext(String s) {
-                // Called each time the observable emits data
-                Log.d("MY OBSERVER", s);
-            }
-        };
-        return myObserver;
     }
 }
